f047a3aa33997c9bda18d3a10035d0b2f5e7c424
Philip <hyngsuki@gmail.com>
2020-11-29 21:14:02 -0500

    Exception handling for adding test

--- app/src/main/java/com/example/brenton_hauth_hyungseoklee_comp304lab4_ex1/TestActivity.java
+++ app/src/main/java/com/example/brenton_hauth_hyungseoklee_comp304lab4_ex1/TestActivity.java
@@ -1,6 +1,7 @@
 package com.example.brenton_hauth_hyungseoklee_comp304lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.brenton_hauth_hyungseoklee_comp304lab4_ex1.helpers.PrefsHelper;
+
+import java.util.List;
 
 public class TestActivity extends AppCompatActivity {
     private EditText editTextTestId, editTextPatientId, editTextNurseId,
@@ -50,18 +53,85 @@ public class TestActivity extends AppCompatActivity {
         editTextBPH = findViewById(R.id.editTextBPH);
         editTextTemperature = findViewById(R.id.editTextTemperature);
         try {
-            test.setTestId(Integer.parseInt(editTextTestId.getText().toString()));
             test.setPatientID(patientId);
             test.setNurseID(nurseId);
-            test.setBPL(Integer.parseInt(editTextBPL.getText().toString()));
-            test.setBPH(Integer.parseInt(editTextBPH.getText().toString()));
-            test.setTemperature(Float.parseFloat(editTextTemperature.getText().toString()));
+            int testId = Integer.parseInt(editTextTestId.getText().toString());
+            int BPL = Integer.parseInt(editTextBPL.getText().toString());
+            int BPH = Integer.parseInt(editTextBPH.getText().toString());
+            if(validate("testId",testId,0))
+            {
+                test.setTestId(testId);
+            }
+            else
+            {
+                Toast.makeText(this.getApplicationContext(),"Entered TestID value(s) is(are) not acceptable.",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(validate("BP",BPL,BPH))
+            {
+                test.setBPL(BPL);
+                test.setBPH(BPH);
+            }
+            else
+            {
+                Toast.makeText(this.getApplicationContext(),"Entered Blood pressure value(s) is(are) not acceptable.",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            float temperature = Float.parseFloat(editTextTemperature.getText().toString());
+            if(validate(temperature))
+            {
+                test.setTemperature(temperature);
+            }
+            else
+            {
+                Toast.makeText(this.getApplicationContext(),"Entered temperature value(s) is(are) not acceptable.",Toast.LENGTH_SHORT).show();
+                return;
+            }
             patientViewModel.insert(test);
             Toast.makeText(this.getApplicationContext(),"Entered test information is successfully stored.",Toast.LENGTH_SHORT).show();
         }
         catch (Exception e)
         {
-         Toast.makeText(this.getApplicationContext(),"Entered value(s) is(are) not acceptable.",Toast.LENGTH_SHORT).show();
+         Toast.makeText(this.getApplicationContext(),"Entered value(s) is(are) not acceptable. Failed to stroe the test data",Toast.LENGTH_SHORT).show();
+         return;
         }
+        Intent intent = new Intent(this,ViewTestInfoActivity.class);
+        intent.putExtra(Patient.PATIENT_ID_EXTRA,patientId);
+        view.getContext().startActivity(intent);
+    }
+    private boolean validate(float f)
+    {
+        if(f > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    private boolean validate(String s, int i1,int i2)
+    {
+        final boolean[] testIdResult = new boolean[1];
+        testIdResult[0] = true;
+        if(s=="BP" && i1 < i2 && i1>0 && i2>0)
+        {
+            return true;
+        }
+        else if(s=="testId" && i1 > 0 && i2 == 0)
+        {
+            patientViewModel.getAllTests().observe(this, new Observer<List<Test>>() {
+                @Override
+                public void onChanged(List<Test> tests) {
+                    for(Test test:tests)
+                    {
+                        if(test.getTestId() == i1)
+                        {
+                            testIdResult[0] = false;
+                            break;
+                        }
+                    }
+                }
+            });
+            return testIdResult[0];
+        }
+        return false;
     }
 }
